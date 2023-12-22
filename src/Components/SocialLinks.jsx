2e2e42d912e0897f70c621b8d48525f1723b3a42
Parsa Tashakkori <theparsius@gmail.com>
2023-12-22 20:58:45 +0330

    [feat] update social links

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -4,19 +4,19 @@ import { icons } from '../Constants/constants';
 function SocialLinks() {
   return (
     <div className="w-full h-[100px] bg-grayscale-950 flex justify-center items-center gap-3 p-6">
-      <a href="https://github.com/bl33h" target="_blank" rel="noreferrer">
+      <a href="https://github.com/theParsius" target="_blank" rel="noreferrer">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faGithub}
         />
       </a>
-      <a href="mailto:sarap.echeverria@gmail.com" target="_blank" rel="noreferrer">
+      <a href="mailto:theparsius@gmail.com" target="_blank" rel="noreferrer">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faEnvelope}
         />
       </a>
-      <a href="https://www.linkedin.com/in/saraech" target="_blank" rel="noreferrer">
+      <a href="https://www.linkedin.com/in/parsa-tashakkori-23b023a3/" target="_blank" rel="noreferrer">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faLinkedin}
