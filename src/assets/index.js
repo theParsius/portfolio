c0f4399a13a000e64c02a749a5103ac2392547cc
Parsa Tashakkori <theparsius@gmail.com>
2023-12-27 16:05:19 +0330

    [feat] reduce image sizes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,65 +1,65 @@
 // Logo
-import theParsiusIcon from './logo/theParsiusLogo.png';
+import theParsiusIcon from "./logo/theParsiusLogo.png";
 
 // Skills
-import unityIcon from './tech/unity.png';
-import csharpIcon from './tech/csharp.png';
-import golangIcon from './tech/golang.png';
-import javaIcon from './tech/java.png';
-import cIcon from './tech/c.png';
-import gitIcon from './tech/git.png';
-import githubIcon from './tech/github.png';
-import psqlIcon from './tech/postgresql.png';
-import pyIcon from './tech/py.png';
-import adobeXdIcon from './tech/adobeXd.png';
-import restApiIcon from './tech/restApi.png';
-import socketIoIcon from './tech/socketIO.png';
-import solanaIcon from './tech/solana.png';
-import close from './close.svg';
-import menu from './menu.svg';
+import unityIcon from "./tech/unity.png";
+import csharpIcon from "./tech/csharp.png";
+import golangIcon from "./tech/golang.png";
+import javaIcon from "./tech/java.png";
+import cIcon from "./tech/c.png";
+import gitIcon from "./tech/git.png";
+import githubIcon from "./tech/github.png";
+import psqlIcon from "./tech/postgresql.png";
+import pyIcon from "./tech/py.png";
+import adobeXdIcon from "./tech/adobeXd.png";
+import restApiIcon from "./tech/restApi.png";
+import socketIoIcon from "./tech/socketIO.png";
+import solanaIcon from "./tech/solana.png";
+import close from "./close.svg";
+import menu from "./menu.svg";
 
 // Projects
 // --- Honeyland
-import honeylandIcon from './projects/honeyland/icon.png';
-import honeylandHive from './projects/honeyland/hive.png';
-import honeylandUniverse from './projects/honeyland/universe.png';
-import honeylandNpc from './projects/honeyland/npc.png';
-import honeylandLand from './projects/honeyland/land.png';
-import honeylandMarketplace from './projects/honeyland/marketplace.png';
-import honeylandUpgrade from './projects/honeyland/upgrade.png';
+import honeylandIcon from "./projects/honeyland/icon.png";
+import honeylandHive from "./projects/honeyland/hive.jpg";
+import honeylandUniverse from "./projects/honeyland/universe.jpg";
+import honeylandNpc from "./projects/honeyland/npc.jpg";
+import honeylandLand from "./projects/honeyland/land.jpg";
+import honeylandMarketplace from "./projects/honeyland/marketplace.jpg";
+import honeylandUpgrade from "./projects/honeyland/upgrade.jpg";
 
 // --- Line-Up
-import lineUpIcon from './projects/lineUp/icon.jpg';
-import lineUpLoading from './projects/lineUp/loading.jpg';
-import lineUpMenu from './projects/lineUp/menu.jpg';
-import lineUpPick from './projects/lineUp/pick.jpg';
-import lineUpFormation from './projects/lineUp/formation.jpg';
-import lineUpMatch from './projects/lineUp/match.jpg';
-import lineUpMatchGoal from './projects/lineUp/match_goal.jpg';
+import lineUpIcon from "./projects/lineUp/icon.jpg";
+import lineUpLoading from "./projects/lineUp/loading.jpg";
+import lineUpMenu from "./projects/lineUp/menu.jpg";
+import lineUpPick from "./projects/lineUp/pick.jpg";
+import lineUpFormation from "./projects/lineUp/formation.jpg";
+import lineUpMatch from "./projects/lineUp/match.jpg";
+import lineUpMatchGoal from "./projects/lineUp/match_goal.jpg";
 
 // --- Funtomime
-import funtomimeIcon from './projects/funtomime/icon.png';
-import funtomimeMenu from './projects/funtomime/menu.png';
-import funtomimeAvatars from './projects/funtomime/avatars.png';
-import funtomimeMatchInfo from './projects/funtomime/match_info.png';
-import funtomimePickTopic from './projects/funtomime/pick_topic.png';
-import funtomimeMatch from './projects/funtomime/match.jpg';
+import funtomimeIcon from "./projects/funtomime/icon.png";
+import funtomimeMenu from "./projects/funtomime/menu.png";
+import funtomimeAvatars from "./projects/funtomime/avatars.png";
+import funtomimeMatchInfo from "./projects/funtomime/match_info.png";
+import funtomimePickTopic from "./projects/funtomime/pick_topic.png";
+import funtomimeMatch from "./projects/funtomime/match.jpg";
 
 // --- Agha Khan
-import aghaKhanIcon from './projects/aghaKhan/icon.png';
-import aghaKhanLoading from './projects/aghaKhan/loading.jpg';
-import aghaKhanMenu from './projects/aghaKhan/menu.jpg';
-import aghaKhanGame from './projects/aghaKhan/game.jpg';
-import aghaKhanConceptCharacter from './projects/aghaKhan/concept_character.jpg';
-import aghaKhanAshou from './projects/aghaKhan/ashou.jpg';
+import aghaKhanIcon from "./projects/aghaKhan/icon.png";
+import aghaKhanLoading from "./projects/aghaKhan/loading.jpg";
+import aghaKhanMenu from "./projects/aghaKhan/menu.jpg";
+import aghaKhanGame from "./projects/aghaKhan/game.jpg";
+import aghaKhanConceptCharacter from "./projects/aghaKhan/concept_character.jpg";
+import aghaKhanAshou from "./projects/aghaKhan/ashou.jpg";
 
 // Stores
-import appStoreIcon from './stores/appStore.png';
-import googlePlayIcon from './stores/googlePlay.png';
-import cafeBazaarIcon from './stores/cafeBazaar.png';
+import appStoreIcon from "./stores/appStore.png";
+import googlePlayIcon from "./stores/googlePlay.png";
+import cafeBazaarIcon from "./stores/cafeBazaar.png";
 
 // Avatar
-import avatar from './avatar/avatar.png';
+import avatar from "./avatar/avatar.png";
 
 export {
   menu,
